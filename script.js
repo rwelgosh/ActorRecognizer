eccc93ffd2665aa6e42d534076e1a37296b98094
Marcus Alenius <marcus.alenius@gmail.com>
2023-06-30 15:08:39 -0400

    added quick demo of how to add box on top of video

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("working...");
+
+function addBox() {
+    const videoContainer = document.getElementsByClassName('watch-video')[0];
+    const box = document.createElement('div');
+    box.style.width = '200px';
+    box.style.height = '200px';
+    box.style.backgroundColor = 'red';
+    box.style.position = 'absolute';
+    box.style.zIndex = '100';
+    videoContainer.appendChild(box);
+}
+
+addBox();
