15ef3019b0eab2b89bb84058f721f6891c59605a
Marcus Alenius <marcus.alenius@gmail.com>
2023-07-29 17:15:52 -0400

    year now displays properly for tv shows

--- inject_script.js
+++ inject_script.js
@@ -87,30 +87,10 @@ chrome.runtime.onMessage.addListener(
                 const displayProductions = removeThisProduction(topFiveProductions, thisProductionTitle);
                 console.log(displayProductions);
 
-                console.log('tv-show year before addInfoCard call', displayProductions[1].year);
-
                 addInfoCard(actorName, actorPlaying, actorImageUrl, actorBio, displayProductions);
 
-
-
-
-
-
-
                 
-            
-                
-            
-            
-                // const topFiveProductions = await getTopFiveProductions(31);
-                // console.log(topFiveProductions);
-            
-                // const movieDuration = await getMovieDuration(13);
-                // console.log(movieDuration);
-            
-                // const lastAirDate = await getLastAirYear(2316);
-                // console.log(lastAirDate);
-            
+                    
             })();
             
 
@@ -211,14 +191,18 @@ async function getTopFiveProductions(actorId, page=1) {
         if ('release_date' in production && production.release_date !== null) {
             const year = production.release_date.slice(0, 4);
             productionData['year'] = year;
-        } else if ('first_air_date' in production && production.first_air_date !== null) {
+        } 
+        else if ('first_air_date' in production && production.first_air_date !== null) {
             const firstAirYear = production.first_air_date.slice(0, 4);
-            // make call to get last air date
-            (async () => {
-                const lastAirYear = await getLastAirYear(production.id);
-                productionData['year'] = firstAirYear + '–' + lastAirYear;
-            })();
-        } else { continue; }
+            productionData['year'] = [firstAirYear, null]; // [firstAirYear, lastAirYear] -- lastAirYear to be called for later
+            // const firstAirYear = production.first_air_date.slice(0, 4);
+            // // make call to get last air date
+            // (async () => {
+            //     const lastAirYear = await getLastAirYear(production.id);
+            //     productionData['year'] = firstAirYear + '–' + lastAirYear;
+            // })();
+        } 
+        else { continue; }
 
         // add popularity
         if ('popularity' in production && production.popularity !== null) { 
@@ -228,6 +212,11 @@ async function getTopFiveProductions(actorId, page=1) {
         // add poster path
         if ('poster_path' in production && production.poster_path !== null) {
             productionData['posterPath'] = production.poster_path;
+        } else { continue; } 
+
+        // add id 
+        if ('id' in production && production.id !== null) {
+            productionData['id'] = production.id;
         } else { continue; } 
 
         // finally, add production object to productions array
@@ -285,117 +274,123 @@ function removeThisProduction(topFiveProductions, thisProductionTitle) {
 // add info card
 function addInfoCard(actorName, actorPlaying, actorImageUrl, actorBio, displayProductions) {
 
-    console.log('tv-show year in addInfoCard', displayProductions[1].year);
+    (async () => {
 
-    const videoContainer = document.getElementsByClassName('watch-video')[0];
-    
-    // card background
-    const infoCardBackground = document.createElement('div');
-    infoCardBackground.classList.add('info-card-background');
-    videoContainer.appendChild(infoCardBackground);
-
-    // card content
-    const infoCardContent = document.createElement('div');
-    infoCardContent.classList.add('info-card-content');
-    videoContainer.appendChild(infoCardContent);
-
-    // card header
-    const cardHeader = document.createElement('div');
-    cardHeader.classList.add('card-header');
-    // card header left
-    const cardHeaderLeft = document.createElement('div');
-    cardHeaderLeft.classList.add('card-header-left');
-    // header image container
-    const headerImageContainer = document.createElement('div');
-    headerImageContainer.classList.add('header-image-container');
-    headerImageContainer.style.backgroundImage = `url(${actorImageUrl})`;
-    cardHeaderLeft.appendChild(headerImageContainer);
-    // header text
-    const headerText = document.createElement('div');
-    headerText.classList.add('header-text');
-    // h1
-    const h1 = document.createElement('h1');
-    const h1Text = document.createTextNode(actorName);
-    h1.appendChild(h1Text);
-    headerText.appendChild(h1);
-    // h2
-    const h2 = document.createElement('h2');
-    const h2Text = document.createTextNode(actorPlaying);
-    h2.appendChild(h2Text);
-    headerText.appendChild(h2);
-    cardHeaderLeft.appendChild(headerText);
-    cardHeader.appendChild(cardHeaderLeft);
-    infoCardContent.appendChild(cardHeader);
-    cardHeaderLeft.insertAdjacentHTML('afterend', '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="10" viewBox="0 0 18 10" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.7071 0.292893L17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711C17.3166 10.0976 16.6834 10.0976 16.2929 9.70711L8.99999 2.41421L1.7071 9.70711C1.31657 10.0976 0.68341 10.0976 0.292886 9.70711C-0.0976385 9.31658 -0.0976388 8.68342 0.292886 8.29289L8.29289 0.292893C8.68341 -0.0976309 9.31658 -0.0976309 9.7071 0.292893Z" fill="white" fill-opacity="0.9"/></svg>');
-    // card header -> done
-
-    // card bio
-    const cardBio = document.createElement('div');
-    cardBio.classList.add('card-bio');
-    // p
-    const p = document.createElement('p');
-    const pText = document.createTextNode(actorBio);
-    p.appendChild(pText);
-    cardBio.appendChild(p);
-    // read more
-    const readMore = document.createElement('div');
-    readMore.classList.add('read-more');
-    // small text - read more
-    const smallTextReadMore = document.createElement('div');
-    smallTextReadMore.classList.add('small-text');
-    const smallTextReadMoreText = document.createTextNode('Read More');
-    smallTextReadMore.appendChild(smallTextReadMoreText);
-    readMore.appendChild(smallTextReadMore);
-    smallTextReadMore.insertAdjacentHTML('afterend', '<svg xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.35226 5.95956L1.10962 10.2022C0.855779 10.456 0.444221 10.456 0.190381 10.2022C-0.0634601 9.94836 -0.0634603 9.5368 0.190381 9.28296L3.9734 5.49994L0.190381 1.71692C-0.0634603 1.46308 -0.0634601 1.05152 0.190381 0.79768C0.444221 0.54384 0.855779 0.543839 1.10962 0.797681L5.35226 5.04032C5.6061 5.29416 5.6061 5.70572 5.35226 5.95956Z" fill="white" fill-opacity="0.6"/></svg>');
-    cardBio.appendChild(readMore);
-    infoCardContent.appendChild(cardBio);
-    // card bio -> done
-
-    // small text – other productions
-    const smallTextOtherProductions = document.createElement('div');
-    smallTextOtherProductions.classList.add('small-text', 'other-productions');
-    const smallTextOtherProductionsText = document.createTextNode('Other Movies and TV Shows');
-    smallTextOtherProductions.appendChild(smallTextOtherProductionsText);
-    infoCardContent.appendChild(smallTextOtherProductions);
-    // small text – other productions -> done
-
-    // productions container
-    const productionsContainer = document.createElement('div');
-    productionsContainer.classList.add('productions-container');
-    // for each display production 
-    for (const displayProduction of displayProductions) {
-        // production
-        const production = document.createElement('div');
-        production.classList.add('production');
-        // production image container
-        const productionImageContainer = document.createElement('div');
-        productionImageContainer.classList.add('production-image-container');
-        productionImageContainer.style.backgroundImage = `url(${poster_image_base_url + displayProduction.posterPath})`;
-        production.appendChild(productionImageContainer);
-        // production text
-        const productionText = document.createElement('div');
-        productionText.classList.add('production-text');
-        // small text - production title
-        const smallTextProductionTitle = document.createElement('div');
-        smallTextProductionTitle.classList.add('small-text', 'production-title');
-        const smallTextProductionTitleText = document.createTextNode(displayProduction.title);
-        smallTextProductionTitle.appendChild(smallTextProductionTitleText);
-        productionText.appendChild(smallTextProductionTitle);
-        // small text - production year
-
+        const videoContainer = document.getElementsByClassName('watch-video')[0];
         
-        const smallTextProductionYear = document.createElement('div');
-        smallTextProductionYear.classList.add('small-text', 'production-year');
-        const smallTextProductionYearText = document.createTextNode(displayProduction.year);
-        smallTextProductionYear.appendChild(smallTextProductionYearText);
-        productionText.appendChild(smallTextProductionYear);
-        production.appendChild(productionText);
-
-        productionsContainer.appendChild(production);
-    }
-    infoCardContent.appendChild(productionsContainer);
-    // productions container -> done 
+        // card background
+        const infoCardBackground = document.createElement('div');
+        infoCardBackground.classList.add('info-card-background');
+        videoContainer.appendChild(infoCardBackground);
     
+        // card content
+        const infoCardContent = document.createElement('div');
+        infoCardContent.classList.add('info-card-content');
+        videoContainer.appendChild(infoCardContent);
+    
+        // card header
+        const cardHeader = document.createElement('div');
+        cardHeader.classList.add('card-header');
+        // card header left
+        const cardHeaderLeft = document.createElement('div');
+        cardHeaderLeft.classList.add('card-header-left');
+        // header image container
+        const headerImageContainer = document.createElement('div');
+        headerImageContainer.classList.add('header-image-container');
+        headerImageContainer.style.backgroundImage = `url(${actorImageUrl})`;
+        cardHeaderLeft.appendChild(headerImageContainer);
+        // header text
+        const headerText = document.createElement('div');
+        headerText.classList.add('header-text');
+        // h1
+        const h1 = document.createElement('h1');
+        const h1Text = document.createTextNode(actorName);
+        h1.appendChild(h1Text);
+        headerText.appendChild(h1);
+        // h2
+        const h2 = document.createElement('h2');
+        const h2Text = document.createTextNode(actorPlaying);
+        h2.appendChild(h2Text);
+        headerText.appendChild(h2);
+        cardHeaderLeft.appendChild(headerText);
+        cardHeader.appendChild(cardHeaderLeft);
+        infoCardContent.appendChild(cardHeader);
+        cardHeaderLeft.insertAdjacentHTML('afterend', '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="10" viewBox="0 0 18 10" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.7071 0.292893L17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711C17.3166 10.0976 16.6834 10.0976 16.2929 9.70711L8.99999 2.41421L1.7071 9.70711C1.31657 10.0976 0.68341 10.0976 0.292886 9.70711C-0.0976385 9.31658 -0.0976388 8.68342 0.292886 8.29289L8.29289 0.292893C8.68341 -0.0976309 9.31658 -0.0976309 9.7071 0.292893Z" fill="white" fill-opacity="0.9"/></svg>');
+        // card header -> done
+    
+        // card bio
+        const cardBio = document.createElement('div');
+        cardBio.classList.add('card-bio');
+        // p
+        const p = document.createElement('p');
+        const pText = document.createTextNode(actorBio);
+        p.appendChild(pText);
+        cardBio.appendChild(p);
+        // read more
+        const readMore = document.createElement('div');
+        readMore.classList.add('read-more');
+        // small text - read more
+        const smallTextReadMore = document.createElement('div');
+        smallTextReadMore.classList.add('small-text');
+        const smallTextReadMoreText = document.createTextNode('Read More');
+        smallTextReadMore.appendChild(smallTextReadMoreText);
+        readMore.appendChild(smallTextReadMore);
+        smallTextReadMore.insertAdjacentHTML('afterend', '<svg xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.35226 5.95956L1.10962 10.2022C0.855779 10.456 0.444221 10.456 0.190381 10.2022C-0.0634601 9.94836 -0.0634603 9.5368 0.190381 9.28296L3.9734 5.49994L0.190381 1.71692C-0.0634603 1.46308 -0.0634601 1.05152 0.190381 0.79768C0.444221 0.54384 0.855779 0.543839 1.10962 0.797681L5.35226 5.04032C5.6061 5.29416 5.6061 5.70572 5.35226 5.95956Z" fill="white" fill-opacity="0.6"/></svg>');
+        cardBio.appendChild(readMore);
+        infoCardContent.appendChild(cardBio);
+        // card bio -> done
+    
+        // small text – other productions
+        const smallTextOtherProductions = document.createElement('div');
+        smallTextOtherProductions.classList.add('small-text', 'other-productions');
+        const smallTextOtherProductionsText = document.createTextNode('Other Movies and TV Shows');
+        smallTextOtherProductions.appendChild(smallTextOtherProductionsText);
+        infoCardContent.appendChild(smallTextOtherProductions);
+        // small text – other productions -> done
+    
+        // productions container
+        const productionsContainer = document.createElement('div');
+        productionsContainer.classList.add('productions-container');
+        // for each display production 
+        for (const displayProduction of displayProductions) {
+            // production
+            const production = document.createElement('div');
+            production.classList.add('production');
+            // production image container
+            const productionImageContainer = document.createElement('div');
+            productionImageContainer.classList.add('production-image-container');
+            productionImageContainer.style.backgroundImage = `url(${poster_image_base_url + displayProduction.posterPath})`;
+            production.appendChild(productionImageContainer);
+            // production text
+            const productionText = document.createElement('div');
+            productionText.classList.add('production-text');
+            // small text - production title
+            const smallTextProductionTitle = document.createElement('div');
+            smallTextProductionTitle.classList.add('small-text', 'production-title');
+            const smallTextProductionTitleText = document.createTextNode(displayProduction.title);
+            smallTextProductionTitle.appendChild(smallTextProductionTitleText);
+            productionText.appendChild(smallTextProductionTitle);
+            // small text - production year
+            if (displayProduction.year.constructor === Array) {
+                const firstAirYear = displayProduction.year[0]
+                // make call to get last air date
+                const lastAirYear = await getLastAirYear(displayProduction.id);
+                displayProduction.year = firstAirYear + '–' + lastAirYear;
+            }
+            const smallTextProductionYear = document.createElement('div');
+            smallTextProductionYear.classList.add('small-text', 'production-year');
+            const smallTextProductionYearText = document.createTextNode(displayProduction.year);
+            smallTextProductionYear.appendChild(smallTextProductionYearText);
+            productionText.appendChild(smallTextProductionYear);
+            production.appendChild(productionText);
+    
+            productionsContainer.appendChild(production);
+        }
+        infoCardContent.appendChild(productionsContainer);
+        // productions container -> done     
+
+    })();
+
 }
 
 
